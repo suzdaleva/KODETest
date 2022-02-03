9d6c1c47c0bc90f7050e7eaa8ed43fbb5bec35ed
Koval Anastasiya <suzdaleva.nastasiya@gmail.com>
2022-02-03 13:18:11 +0300

    unnecessary code deleted

--- app/src/main/java/com/manicpixie/kodetest/presentation/main/MainScreen.kt
+++ app/src/main/java/com/manicpixie/kodetest/presentation/main/MainScreen.kt
@@ -676,25 +676,5 @@ fun Modifier.clearFocusOnKeyboardDismiss(): Modifier = composed {
 }
 
 
-private class FlingBehaviourMultiplier(
-    private val multiplier: Float,
-    private val baseFlingBehavior: FlingBehavior
-) : FlingBehavior {
-    override suspend fun ScrollScope.performFling(initialVelocity: Float): Float {
-        return with(baseFlingBehavior) {
-            performFling(initialVelocity * multiplier)
-        }
-    }
-}
-
-@Composable
-fun rememberFlingBehaviorMultiplier(
-    multiplier: Float,
-    baseFlingBehavior: FlingBehavior
-): FlingBehavior = remember(multiplier, baseFlingBehavior) {
-    FlingBehaviourMultiplier(multiplier, baseFlingBehavior)
-}
-
-
 
 
